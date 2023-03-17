f703c472234ed449962cb5485d161c5ec82bffa2
dhumal dhanraj <dhanraj.sdhumal@gmail.com>
2023-03-17 21:14:29 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatBotHomeComponent } from './chat-bot-home/chat-bot-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatBotHomeComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/chat-bot-home/chat-bot-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-bot-home',
+  templateUrl: './chat-bot-home.component.html',
+  styleUrls: ['./chat-bot-home.component.scss']
+})
+export class ChatBotHomeComponent {
+
+}
